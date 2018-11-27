8f1bc8e9beca08a47fb3408e803f62189ea344b3
Gonçalo Branco Lourenço <2160838@my.ipleiria.pt>
2018-11-27 16:43:06 +0000

    Vuetify

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,15 +1,30 @@
 @extends('master')
 
-@section('title', 'Vue.js App')
+@section('title', 'Restaurant Management')
 
 @section('content')
-    <div class="jumbotron">
-        <h1>@{{ title }}</h1>
-    </div>
-    <router-link to="/items">Items</router-link> -  
-    <router-view></router-view>
+<v-app>
+    <v-toolbar>
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-toolbar-title>@{{title}}</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+        <template v-if="user==null">
+            <v-btn to="/items" flat>Items</v-btn>
+            <v-btn flat>Login</v-btn>
+            <v-btn flat>Register</v-btn>
+        </template>
+        <template v-if="user">
+        
+        </template>                   
+        </v-toolbar-items>
+    </v-toolbar>
+</v-app>
+<router-view></router-view>
 @endsection
 
 @section('pagescript')
-<script src="js/app.js"></script>
+<script src="js/app.js"></script>    
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script> 
+<script src="https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.js"></script>
  @stop  
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,43 +1,21 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>@yield('title')</title>
-        @yield('extrastyles') 
-        <!-- Latest compiled and minified CSS & JS -->
-        <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons' rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
-
-    </head> 
-    <body>  
-        <div class="container" id="app">
-            <v-app>
-                <v-toolbar>
-                    <v-toolbar-side-icon></v-toolbar-side-icon>
-                    <v-toolbar-title>@{{title}}</v-toolbar-title>
-                    <v-spacer></v-spacer>
-                    <v-toolbar-items class="hidden-sm-and-down">
-                    <template v-if="user==null">
-                      <v-btn flat>Items</v-btn>
-                      <v-btn flat>Login</v-btn>
-                      <v-btn flat>Register</v-btn>
-                    </template>
-                    <template v-if="user">
-                      
-                    </template>                   
-                    </v-toolbar-items>
-                  </v-toolbar>
-            </v-app>
-            @yield('content')
-        </div>
-
-     @yield('pagescript')  
-     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>  
-    <script src="https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.js"></script>
-  <script>    
-    </body>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title')</title>
+    @yield('extrastyles') 
+    <!-- Latest compiled and minified CSS & JS -->
+    <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons' rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
+  </head> 
+  <body>  
+    <div class="container" id="app">
+      @yield('content')
+    </div>
+    @yield('pagescript') 
+  </body>
 </html>
