c793680693e34d6529ea19158c33a7681bed6f68
Gonçalo Branco Lourenço <2160838@my.ipleiria.pt>
2018-11-28 12:01:23 +0000

    US1

--- resources/js/components/items/itemList.vue
+++ resources/js/components/items/itemList.vue
@@ -1,25 +1,28 @@
 <template>
-  <template v-for="(item, index) in items">
-    <v-flex xs12 sm6 offset-sm3 v-bind:key="index">
-      <v-card>
-        <v-img src="https://cdn.vuetifyjs.com/images/cards/desert.jpg" aspect-ratio="2.75"></v-img>
+  <div class="container">
+    <template v-for="(item, index) in items" style="margin-bottom: 5px">
+      <v-flex xs12 sm6 offset-sm3 v-bind:key="index">
+        <v-card>
+          <v-img :src="urlToImage(item)" aspect-ratio="2.75"></v-img>
 
-        <v-card-title primary-title>
-          <div>
-            <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
-            <div>Located two hours south of Sydney in the
-              <br>Southern Highlands of New South Wales, ...
+          <v-card-title primary-title>
+            <div>
+              <h2 class="headline mb-0">{{item.name}}</h2>
+              <div>
+                {{item.type}}
+                <br>
+                {{item.description}}
+              </div>
             </div>
-          </div>
-        </v-card-title>
-
-        <v-card-actions>
-          <v-btn flat color="orange">Share</v-btn>
-          <v-btn flat color="orange">Explore</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </template>
+          </v-card-title>
+          <!--<v-card-actions>
+            <v-btn flat color="orange">Share</v-btn>
+            <v-btn flat color="orange">Explore</v-btn>
+          </v-card-actions>-->
+        </v-card>
+      </v-flex>
+    </template>
+  </div>
 </template>
 <script>
 export default {
@@ -27,7 +30,11 @@ export default {
     return {};
   },
   props: ["items"],
-  methods: {},
+  methods: {
+    urlToImage: function(item) {
+      return "/storage/items/" + item.photo_url + ".jpg";
+    }
+  },
   mounted() {
     console.log("Component mounted.");
   }
