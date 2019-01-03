a65f981582fecd7d9cb28521fce3a4926c5d2b70
Gonçalo Lourenço <goncalo.blourenc@gmail.com>
2019-01-03 13:10:57 +0000

    Fim das US do waiter (sem websockets)

---
+++ resources/js/components/waiter/mealDetails.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-dialog max-width="500px">
+    <v-card>
+      <v-card-title>
+        <span class="headline">Meal Summary</span>
+      </v-card-title>
+      <v-card-text>
+        <v-container grid-list-md>
+          <v-layout wrap></v-layout>
+        </v-container>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue darken-1" flat @click="closeDialog">Close</v-btn>
+        <v-btn color="blue darken-1" flat @click="terminateOrder">Terminate order</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    meal: Object
+  },
+  methods: {
+    closeDialog: function() {
+      this.$emit("close-dialog");
+    },
+    terminateOrder: function() {
+      this.$emit("terminate-order");
+    }
+  }
+};
+</script>
